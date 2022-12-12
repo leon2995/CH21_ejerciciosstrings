8513b2fd131003bc740cb223bedcec0a1185a7ef
leon2995 <lallo042994@gmail.com>
2022-12-12 13:48:46 -0600

    se soluciona el primer ejercicio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let cadena = "abraham"
+let indices = [];
+for(let i = 0; i < cadena.length; i++) {
+	if (cadena[i].charAt() === "a") indices.push(i);
+}
+console.log(indices);
+//primer ejercicio
+
